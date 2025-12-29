a63e9fb8f36d3235956df160c6c4cd2fa2061e05
rL <lagoszrafal@gmail.com>
2025-12-29 18:46:52 +0100

    first commit

---
+++ projekt-zaliczeniowy/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace projekt_zaliczeniowy.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
